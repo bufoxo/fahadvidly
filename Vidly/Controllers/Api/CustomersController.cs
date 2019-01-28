aeda97d673152b59e9822c1c0918cb38f6b49b19
fahad ali <fahad.arahim@outlook.com>
2019-01-28 07:05:47 +0300

    srating the project deom the azure
    customer and movie api

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Web;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.Dtos;
+using  Vidly.Models;
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+
+            _context = new ApplicationDbContext();
+            ;
+        }
+        //get /api/customers  
+        public IHttpActionResult GetCustomers ()
+        {
+            var customerDtos = 
+                 _context.Customers
+                .Include(c => c.MembershipType)
+                .ToList()
+                .Select(Mapper.Map<Customer, CustomerDto>);
+
+            return Ok(customerDtos);
+        } 
+        //Get/api/customers/1
+         
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return NotFound();   
+         return   Ok(Mapper.Map<Customer,CustomerDto>(customer ));
+        } 
+
+        //Post/api/customers
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                customerDto.Id = customer.Id;
+                return Created(new Uri(Request.RequestUri+"/"+ customer.Id),customerDto );
+
+        }
+
+        //Put /api/customerDto /1
+        [HttpPut]
+        public IHttpActionResult UpdateCustomer(int id, CustomerDto customerDto)
+        {
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                return BadRequest();
+
+            Mapper.Map(customerDto, customerInDb);
+          
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+        //DELETE 
+        [HttpDelete]
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+                _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+
+            return Ok();
+
+
+        }
+    }
+
+    
+}
---
+++ Vidly/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Profile;
+using System.ComponentModel.DataAnnotations;
+using System.Web.UI.WebControls;
+
+namespace Vidly.Models
+{
+    public class Customer
+    { 
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+      
+        public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "membership Type ")]
+        public byte MembershipTypeId { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
+        public DateTime?  Birthdate { get; set; }
+    }
+}
