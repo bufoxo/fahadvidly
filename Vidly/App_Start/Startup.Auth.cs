e123b41e2b3dcb1f6f98a4485a390d80150341ae
fahad ali <fahad.arahim@outlook.com>
2019-02-03 07:59:12 +0300

    Move Facebook app settings to web.config

--- Vidly/App_Start/Startup.Auth.cs
+++ Vidly/App_Start/Startup.Auth.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
@@ -55,8 +56,8 @@ namespace Vidly
             //   consumerSecret: "");
 
             app.UseFacebookAuthentication(
-               appId: "1064625440399873",
-               appSecret: "c9c6cf539e3e5ebda493f67af393dcc5");
+               appId: ConfigurationManager.AppSettings["FacebookAppId"],
+               appSecret: ConfigurationManager.AppSettings["FacebookAppSecret"];
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
